940a49a4863547fee271126aff65118de9e0bc1a
eastolfi <astolfi_wotc@hotmail.com>
2016-08-04 10:26:28 +0000

    test: Add browser test

---
+++ test/browser/app.js
@@ -0,0 +1,20 @@
+/* global chai, _ */
+
+var expect = chai.expect;
+
+var MongoPortable = null,
+    Logger = null;
+
+describe("MongoPortable - Web", function() {
+    it("should have the dependencies ready", function() {
+        expect(require).to.exist;
+        expect(_).to.exist;
+        
+        MongoPortable = require("mongo-portable");
+        Logger = require("jsw-logger");
+        
+        expect(MongoPortable).to.exist;
+        expect(Logger).to.exist;
+    });
+});
+    
