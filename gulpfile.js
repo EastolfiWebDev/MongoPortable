e26b756c3d64f882974f7cd748432c1d5240d460
eastolfi <astolfi_wotc@hotmail.com>
2016-10-16 13:24:58 +0000

    chore(building): Change grunt for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,16 @@ var gutil = require('gulp-util');
 var git = require('gulp-git');
 var fs = require('fs');
 var minimist = require('minimist');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
+var mocha = require('gulp-mocha');
+var mochaPhantomJS = require('gulp-mocha-phantomjs');
+// var browserify = require('gulp-browserify');
+var jsdoc = require('gulp-jsdoc3');
+var gulpJsdoc2md = require('gulp-jsdoc-to-markdown');
+var rename = require('gulp-rename');
+var jsdoc2md = require('jsdoc-to-markdown');
 
 var knownOptions = {
     boolean: ['major', 'minor', 'patch'],
@@ -15,6 +25,74 @@ var knownOptions = {
 };
 
 var options = minimist(process.argv.slice(2), knownOptions);
+
+gulp.task('doc:api:full', function () {
+    return jsdoc2md.render({ files: 'src/**/*.js' })
+    .then(function(output) {
+        return fs.writeFileSync('api/documentation.md', output);
+    });
+});
+
+gulp.task('doc:api', function () {
+    return gulp.src(['src/MongoPortable.js', 'src/Collection.js', 'src/Cursor.js', 'src/Selector.js', 'src/ObjectId.js'])
+        .pipe(gulpJsdoc2md(/*{ template: fs.readFileSync('./readme.hbs', 'utf8') }*/))
+        .on('error', function (err) {
+            gutil.log(gutil.colors.red('jsdoc2md failed'), err.message);
+        })
+        .pipe(rename(function (path) {
+            path.extname = '.md';
+        }))
+        .pipe(gulp.dest('api'));
+});
+
+gulp.task('doc:app', function (cb) {
+    var config = require('./jsdoc.conf.json');
+    
+    gulp.src(['./src/**/*.js'], {read: false})
+        .pipe(jsdoc(config, cb));
+});
+
+// TODO
+// gulp.task('bundle', function() {
+//     // Single entry point to browserify 
+//     gulp.src('src/js/app.js')
+//         .pipe(browserify({
+//           insertGlobals : true,
+//           debug : !gulp.env.production
+//         }))
+//         .pipe(gulp.dest('./build/js'))
+// });
+
+gulp.task('clean:lib', function () {
+    return del([
+        'lib/**/*.js',
+        // // here we use a globbing pattern to match everything inside the `mobile` folder
+        // 'dist/mobile/**/*',
+        // // we don't want to clean this file though so we negate the pattern
+        // '!dist/mobile/deploy.json'
+    ]);
+});
+
+gulp.task('test:app', function () { 
+    return gulp.src('test/*.js', {read: false})
+        // gulp-mocha needs filepaths so you can't have any plugins before it
+        .pipe(mocha({reporter: 'nyan'}));
+});
+
+gulp.task('test:browser', function () { 
+    return gulp.src('test/browser/index.html', {read: false})
+        .pipe(mochaPhantomJS({reporter: 'nyan'}));
+});
+
+gulp.task('build:app', function () {
+    return gulp.src('src/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('lib'));
+});
 
 gulp.task('changelog', function () {
     return gulp.src('CHANGELOG.md', {
@@ -26,7 +104,7 @@ gulp.task('changelog', function () {
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('github-release', function(done) {
+gulp.task('release:github', function (done) {
     conventionalGithubReleaser({
         type: "oauth",
         token: 'bb2128b43719d95467e61a861c079152986ef9ba' // change this to your own GitHub token or use an environment variable
@@ -86,26 +164,79 @@ gulp.task('create-new-tag', function (cb) {
     }
 });
 
+gulp.task('build', function(callback) {
+    runSequence(
+        'build:app',
+        // 'bundle:app',
+    function (error) {
+        if (error) {
+            console.log(error.message);
+        }
+        
+        callback(error);
+    }
+    );
+});
+
+gulp.task('test', function(callback) {
+    runSequence(
+        'test:app',
+        'test:browser',
+    function (error) {
+        if (error) {
+            console.log(error.message);
+        }
+        
+        callback(error);
+    }
+    );
+});
+
+gulp.task('doc', function(cb) {
+    runSequence(
+        'doc:app',
+        'doc:api',
+        'doc:api:full',
+    function(error) {
+        if (error) {
+            console.log(error.message);
+        }
+        
+        cb(error);
+    });
+});
+
+gulp.task('generate', function(cb) {
+    runSequence(
+        'build',
+        'test',
+        'doc',
+    function(error) {
+        if (error) {
+            console.log(error.message);
+        }
+        
+        cb(error);
+    });
+});
+
 gulp.task('release', function (callback) {
     runSequence(
-        // run test on souºrce
-        // compile
-        // run test on build
-        // bundle
-        // run browser test
-        // add build/browser
-        'version',
-        'changelog',
-        'commit-changes',
-        'push-changes',
-        'create-new-tag',
-        'github-release',
+        'generate',
+        // git add build/browser
+        // 'version',
+        // 'changelog',
+        // 'commit-changes',
+        // 'push-changes',
+        // 'create-new-tag',
+        // 'github-release',
     function (error) {
         if (error) {
             console.log(error.message);
         } else {
             console.log('RELEASE FINISHED SUCCESSFULLY');
         }
+        
         callback(error);
     });
 });
