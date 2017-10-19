b7f640f478b9d75a2717c16cac4ec77aa1d7fba1
Eduardo Astolfi <eduardo.astolfi@prima-solutions.com>
2017-10-19 15:51:06 +0200

    test: Update Aggregation tests

--- src/aggregation/index.js
+++ src/aggregation/index.js
@@ -1,5 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var Aggregation_1 = require("./Aggregation");
 exports.Aggregation = Aggregation_1.Aggregation;
-
 //# sourceMappingURL=index.js.map
