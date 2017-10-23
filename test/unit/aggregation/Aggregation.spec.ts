8780a317740bcefec74bafc44a9ad012edae8c06
eastolfi <astolfi_wotc@hotmail.com>
2017-10-23 19:25:40 +0000

    test: Fix aggregation tests

--- test/unit/aggregation/Aggregation.spec.ts
+++ test/unit/aggregation/Aggregation.spec.ts
@@ -1,5 +1,6 @@
 import "mocha";
 import { expect } from "chai";
+import * as Promise from "promise";
 
 import { TestHelper } from "../../helper/index";
 import { MongoPortable } from "../../../src/core/index";
@@ -44,251 +45,272 @@ describe("Aggregation", function() {
     });
     
     describe("#Group", function() {
-        it("should be able to group several documents ($sum)", function() {
-            var coll = db.collection("coll_group_1");
-            
-            coll.insert({
-                _id: 1111,
-                name: 'eeee',
-                age: 22
-            }, {chain: true})
-            .insert({
-                _id: 1112,
-                name: 'dddd',
-                age: 23
-            }, {chain: true})
-            .insert({
-                _id: 1113,
-                name: 'cccc',
-                age: 22
-            }, {chain: true})
-            .insert({
-                _id: 1114,
-                name: 'bbbb',
-                age: 25
-            }, {chain: true})
-            .insert({
-                _id: 1115,
-                name: 'aaaa',
-                age: 23
+        it("should be able to group several documents ($sum)", function(done) {
+            db.collection("coll_group_1").then(coll => {
+                Promise.all([
+                    coll.insert({
+                        _id: 1111,
+                        name: 'eeee',
+                        age: 22
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1112,
+                        name: 'dddd',
+                        age: 23
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1113,
+                        name: 'cccc',
+                        age: 22
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1114,
+                        name: 'bbbb',
+                        age: 25
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1115,
+                        name: 'aaaa',
+                        age: 23
+                    })
+                ]).then(() => {
+                    var docs = coll.aggregate([{
+                        $group: {
+                            _id: "$age",
+                            count: {
+                                $sum: 1
+                            },
+                            total_id: {
+                                $sum: "$_id"
+                            },
+                            total_age: {
+                                $sum: '$age'
+                            }
+                        }
+                    }]);
+                    
+                    expect(docs).to.exist;
+                    expect(docs).to.be.instanceof(Array);
+                    expect(docs).to.have.length(3);
+                    
+                    done();
+                });
             });
-            
-            var docs = coll.aggregate([{
-                $group: {
-                    _id: "$age",
-                    count: {
-                        $sum: 1
-                    },
-                    total_id: {
-                        $sum: "$_id"
-                    },
-                    total_age: {
-                        $sum: '$age'
-                    }
-                }
-            }]);
-            
-            expect(docs).to.exist;
-            expect(docs).to.be.instanceof(Array);
-            expect(docs).to.have.length(3);
         });
         
-        it("should be able to group several documents ($avg)", function() {
-            var coll = db.collection("coll_group_2");
-            
-            coll.insert({
-                _id: 1111,
-                name: 'eeee',
-                age: 22
-            }, {chain: true})
-            .insert({
-                _id: 1112,
-                name: 'dddd',
-                age: 23
-            }, {chain: true})
-            .insert({
-                _id: 1113,
-                name: 'cccc',
-                age: 22
-            }, {chain: true})
-            .insert({
-                _id: 1114,
-                name: 'bbbb',
-                age: 25
-            }, {chain: true})
-            .insert({
-                _id: 1115,
-                name: 'aaaa',
-                age: 23
+        it("should be able to group several documents ($avg)", function(done) {
+            db.collection("coll_group_2").then(coll => {
+                Promise.all([
+                    coll.insert({
+                        _id: 1111,
+                        name: 'eeee',
+                        age: 22
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1112,
+                        name: 'dddd',
+                        age: 23
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1113,
+                        name: 'cccc',
+                        age: 22
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1114,
+                        name: 'bbbb',
+                        age: 25
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1115,
+                        name: 'aaaa',
+                        age: 23
+                    })
+                ]).then(() => {
+                    var docs = coll.aggregate([{
+                        $group: {
+                            _id: null,
+                            aver_age: {
+                                $avg: '$age'
+                            }
+                        }
+                    }]);
+                    
+                    expect(docs).to.exist;
+                    expect(docs).to.be.instanceof(Array);
+                    expect(docs).to.have.length(1);
+                    expect(docs[0].aver_age).to.be.equal(23);
+                    
+                    done();
+                });
             });
-            
-            var docs = coll.aggregate([{
-                $group: {
-                    _id: null,
-                    aver_age: {
-                        $avg: '$age'
-                    }
-                }
-            }]);
-            
-            expect(docs).to.exist;
-            expect(docs).to.be.instanceof(Array);
-            expect(docs).to.have.length(1);
-            expect(docs[0].aver_age).to.be.equal(23);
         });
     });
     
     describe("#Match", function() {
-        it("should be able to match documents", function() {
-            var coll = db.collection("coll_match_1");
-            
-            coll.insert({
-                _id: 1111,
-                name: 'eeee',
-                age: 22
-            }, {chain: true})
-            .insert({
-                _id: 1112,
-                name: 'dddd',
-                age: 23
-            }, {chain: true})
-            .insert({
-                _id: 1113,
-                name: 'cccc',
-                age: 22
-            }, {chain: true})
-            .insert({
-                _id: 1114,
-                name: 'bbbb',
-                age: 25
-            }, {chain: true})
-            .insert({
-                _id: 1115,
-                name: 'aaaa',
-                age: 23
-            });
-            
-            var docs = coll.aggregate([{
-                $match: {
-                    age: {
-                        $gte: 23
+        it("should be able to match documents", function(done) {
+            db.collection("coll_match_1").then(coll => {
+                Promise.all([
+                    coll.insert({
+                        _id: 1111,
+                        name: 'eeee',
+                        age: 22
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1112,
+                        name: 'dddd',
+                        age: 23
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1113,
+                        name: 'cccc',
+                        age: 22
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1114,
+                        name: 'bbbb',
+                        age: 25
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1115,
+                        name: 'aaaa',
+                        age: 23
+                    })
+                ]);
+                
+                var docs = coll.aggregate([{
+                    $match: {
+                        age: {
+                            $gte: 23
+                        }
                     }
-                }
-            }]);
-            
-            expect(docs).to.exist;
-            expect(docs).to.be.instanceof(Array);
-            expect(docs).to.have.length(3);
+                }]);
+                
+                expect(docs).to.exist;
+                expect(docs).to.be.instanceof(Array);
+                expect(docs).to.have.length(3);
+                
+                done();
+            });
         });
     });
     
     describe("#Sort", function() {
-        it("should be able to sort documents", function() {
-            var coll = db.collection("coll_sort_1");
+        it("should be able to sort documents", function(done) {
+            db.collection("coll_sort_1").then(coll => {
+                Promise.all([
+                    coll.insert({
+                        _id: 1111,
+                        name: 'eeee',
+                        age: 22
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1112,
+                        name: 'dddd',
+                        age: 23
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1113,
+                        name: 'cccc',
+                        age: 22
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1114,
+                        name: 'bbbb',
+                        age: 25
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1115,
+                        name: 'aaaa',
+                        age: 23
+                    })
+                ]);
             
-            coll.insert({
-                _id: 1111,
-                name: 'eeee',
-                age: 22
-            }, {chain: true})
-            .insert({
-                _id: 1112,
-                name: 'dddd',
-                age: 23
-            }, {chain: true})
-            .insert({
-                _id: 1113,
-                name: 'cccc',
-                age: 22
-            }, {chain: true})
-            .insert({
-                _id: 1114,
-                name: 'bbbb',
-                age: 25
-            }, {chain: true})
-            .insert({
-                _id: 1115,
-                name: 'aaaa',
-                age: 23
+                var docs = coll.aggregate([{
+                    $sort: {
+                        age: -1
+                    }
+                }]);
+                
+                expect(docs).to.exist;
+                expect(docs).to.be.instanceof(Array);
+                expect(docs).to.have.length(5);
+                
+                done();
             });
-            
-            var docs = coll.aggregate([{
-                $sort: {
-                    age: -1
-                }
-            }]);
-            
-            expect(docs).to.exist;
-            expect(docs).to.be.instanceof(Array);
-            expect(docs).to.have.length(5);
         });
     });
     
     describe("#Mixed", function() {
-        it("should be able to aggregate several stages", function() {
-            var coll = db.collection("coll_mixed_1");
-            
-            coll.insert({
-                _id: 1111,
-                name: 'eeee',
-                age: 22
-            }, {chain: true})
-            .insert({
-                _id: 1112,
-                name: 'dddd',
-                age: 23
-            }, {chain: true})
-            .insert({
-                _id: 1113,
-                name: 'cccc',
-                age: 22
-            }, {chain: true})
-            .insert({
-                _id: 1114,
-                name: 'bbbb',
-                age: 25
-            }, {chain: true})
-            .insert({
-                _id: 1115,
-                name: 'aaaa',
-                age: 23
-            }, {chain: true})
-            .insert({
-                _id: 1115,
-                name: 'abab',
-                age: 22
+        it("should be able to aggregate several stages", function(done) {
+            db.collection("coll_mixed_1").then(coll => {
+                Promise.all([
+                    coll.insert({
+                        _id: 1111,
+                        name: 'eeee',
+                        age: 22
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1112,
+                        name: 'dddd',
+                        age: 23
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1113,
+                        name: 'cccc',
+                        age: 22
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1114,
+                        name: 'bbbb',
+                        age: 25
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1115,
+                        name: 'aaaa',
+                        age: 23
+                    }, {chain: true}),
+                    coll.insert({
+                        _id: 1115,
+                        name: 'abab',
+                        age: 22
+                    })
+                ]);
+                
+                
+                var docs = coll.aggregate([{
+                    $match: {
+                        age: {
+                            $lt: 25
+                        }
+                    }
+                }, {
+                    $group: {
+                        _id: "$age",
+                        total: {
+                            $sum: 1
+                        }
+                    }
+                }, {
+                    $sort: {
+                        total: -1
+                    }
+                }, {
+                    $project: {
+                        suma_total: '$total',
+                        total: -1
+                    }
+                }]);
+                
+                expect(docs).to.exist;
+                expect(docs).to.be.instanceof(Array);
+                expect(docs).to.have.length(2);
+                
+                expect(docs[0]).to.be.eql({ _id: 22, suma_total: 3 });
+                expect(docs[1]).to.be.eql({ _id: 23, suma_total: 2 });
+                
+                done();
             });
-            
-            var docs = coll.aggregate([{
-                $match: {
-                    age: {
-                        $lt: 25
-                    }
-                }
-            }, {
-                $group: {
-                    _id: "$age",
-                    total: {
-                        $sum: 1
-                    }
-                }
-            }, {
-                $sort: {
-                    total: -1
-                }
-            }, {
-                $project: {
-                    suma_total: '$total',
-                    total: -1
-                }
-            }]);
-            
-            expect(docs).to.exist;
-            expect(docs).to.be.instanceof(Array);
-            expect(docs).to.have.length(2);
-            
-            expect(docs[0]).to.be.eql({ _id: 22, suma_total: 3 });
-            expect(docs[1]).to.be.eql({ _id: 23, suma_total: 2 });
         });
     });
 });
